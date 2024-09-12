87e00ce60a88e2ab380eceb8a42b7017fd74261e
Michaela Paige <114429586+MichaelaP18@users.noreply.github.com>
2024-09-12 15:51:11 -0600

    Adding github link

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function StudentInfo() {
     return (
       <div>
         <h1>Michaela Paige</h1>
-        <p>Link to github repository</p>
+        <Link href="https://github.com/MichaelaP18/cprg306-assignments">GitHub.com</Link>
       </div>
     );
 }
