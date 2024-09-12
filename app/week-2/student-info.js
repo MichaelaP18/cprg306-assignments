16102ac758c957c8cb698341d8378d3e38df972e
Michaela Paige <114429586+MichaelaP18@users.noreply.github.com>
2024-09-12 15:55:13 -0600

    Link update

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,7 @@ export default function StudentInfo() {
     return (
       <div>
         <h1>Michaela Paige</h1>
-        <Link href="https://github.com/MichaelaP18/cprg306-assignments">GitHub.com</Link>
+        <Link href="https://github.com/MichaelaP18">GitHub.com</Link>
       </div>
     );
 }
