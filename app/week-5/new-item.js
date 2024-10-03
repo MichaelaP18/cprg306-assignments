399b7dd8c75836d8508c102f586f7b2d59fec0da
Michaela Paige <114429586+MichaelaP18@users.noreply.github.com>
2024-10-03 16:09:28 -0600

    Week 5 creation

---
+++ app/week-5/new-item.js
@@ -0,0 +1,81 @@
+"use client";
+
+import React, { useState } from 'react';
+
+export default function NewItem() {
+const [quantity, setQuantity] = useState(1);
+const [name, setName] = useState("");
+const [category, setCategory] = useState("Produce");
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+}
+
+const item = {
+    name, 
+    quantity,
+    category,
+};
+
+console.log(item);
+
+alert(`Name: ${name}, Quantity: ${quantity}, Category: ${category}`);
+
+setName("");
+setQuantity("");
+setCategory("");
+
+    const Increment = () => {
+        setQuantity(prevQuantity => {
+            if (prevQuantity < 20) {
+                return prevQuantity + 1;
+            }
+            return prevQuantity;
+        });
+    }
+
+    const Decrement = () => {
+        setQuantity(prevQuantity => {
+            if (prevQuantity > 1) {
+                return prevQuantity - 1;
+            }
+            return prevQuantity;
+        });
+    }
+
+    return (
+        <form>
+        <div>
+            <label>
+                Name: 
+                <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+            </label>
+            </div>
+            <div>
+            <p>Quantity: {quantity}</p> 
+            <button className="bg-green-400 hover:bg-green-600 text-write w-30 rounded p-1 m-2" onClick={Increment}>Increment Quantity</button>
+
+            <button className="bg-green-400 hover:bg-green-600 text-write w-30 rounded p-1 m-2" onClick={Decrement}>Decrement Quantity</button>
+        </div>
+        <div>
+            <label>
+                Category:
+            </label>
+                <select id="category" value={category} onChange={(e) => setCategory(e.target.value)}>
+                    <option value="Produce">Produce</option>
+                    <option value="Dairy">Dairy</option>
+                    <option value="Bakery">Bakery</option>
+                    <option value="Meat">Meat</option>
+                    <option value="Frozen Foods">Frozen Foods</option>
+                    <option value="Canned Goods">Canned Goods</option>
+                    <option value="Dry Goods">Dry Goods</option>
+                    <option value="Beverages">Beverages</option>
+                    <option value="Snacks">Snacks</option>
+                    <option value="Household">Household</option>
+                    <option value="Other">Other</option>
+                </select>
+        </div>
+        <button type="Submit" onSubmit={handleSubmit}>Submit</button>
+    </form>
+    );
+}
